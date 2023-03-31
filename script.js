8812881d8625b2ec2e9fde721623a768f194c2d3
heitorcalaca <31066565+heitorcalaca@users.noreply.github.com>
2023-03-30 21:22:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,12 +29,16 @@ const d44 = document.getElementById("44");
 const d45 = document.getElementById("45");
 const d46 = document.getElementById("46");
 const d47 = document.getElementById("47");
+var arrayChech = [];
+
+const i18 = document.getElementById("i18");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkInputs();
   getCheckValues();
+  console.log(arrayChech);
 });
 
 function checkInputs() {
@@ -51,6 +55,11 @@ function checkInputs() {
     setErrorFor(pacName, "O nome do paciente é obrogatório");
   } else {
     setSuccessFor(pacName);
+  }
+
+  if (arrayChech.length == 0) {
+    setCheckErrorFor(document.getElementsByClassName("form-check"));
+    console.log("erro");
   }
 }
 
@@ -72,89 +81,133 @@ function setErrorFor(input, message) {
   formControl.className = "form-control error";
 }
 
+function setCheckSuccessFor(input) {
+  const checkControl = input;
+
+  checkControl.className = "form-check success";
+}
+
+function setCheckErrorFor(input) {
+  const checkControl = input;
+
+  checkControl.className = "form-check error";
+}
+
 function getCheckValues() {
+  if (i18.checked) {
+    console.log("18 checado");
+  }
+
   if (d11.checked) {
     console.log("11 checado");
+    arrayChech.push("11");
   }
   if (d12.checked) {
     console.log("12 checado");
+    arrayChech.push("12");
   }
   if (d13.checked) {
     console.log("13 checado");
+    arrayChech.push("13");
   }
   if (d14.checked) {
     console.log("14 checado");
+    arrayChech.push("14");
   }
   if (d15.checked) {
     console.log("15 checado");
+    arrayChech.push("15");
   }
   if (d16.checked) {
     console.log("16 checado");
+    arrayChech.push("16");
   }
   if (d17.checked) {
     console.log("17 checado");
+    arrayChech.push("17");
   }
   if (d21.checked) {
     console.log("21 checado");
+    arrayChech.push("21");
   }
   if (d22.checked) {
     console.log("22 checado");
+    arrayChech.push("22");
   }
   if (d23.checked) {
     console.log("23 checado");
+    arrayChech.push("23");
   }
   if (d24.checked) {
     console.log("24 checado");
+    arrayChech.push("24");
   }
   if (d25.checked) {
     console.log("25 checado");
+    arrayChech.push("25");
   }
   if (d26.checked) {
     console.log("26 checado");
+    arrayChech.push("26");
   }
   if (d27.checked) {
     console.log("27 checado");
+    arrayChech.push("27");
   }
   if (d31.checked) {
     console.log("31 checado");
+    arrayChech.push("31");
   }
   if (d32.checked) {
     console.log("32 checado");
+    arrayChech.push("32");
   }
   if (d33.checked) {
     console.log("33 checado");
+    arrayChech.push("33");
   }
   if (d34.checked) {
     console.log("34 checado");
+    arrayChech.push("34");
   }
   if (d35.checked) {
     console.log("35 checado");
+    arrayChech.push("35");
   }
   if (d36.checked) {
     console.log("36 checado");
+    arrayChech.push("36");
   }
   if (d37.checked) {
     console.log("37 checado");
+    arrayChech.push("37");
   }
   if (d41.checked) {
     console.log("41 checado");
+    arrayChech.push("41");
   }
   if (d42.checked) {
     console.log("42 checado");
+    arrayChech.push("42");
   }
   if (d43.checked) {
     console.log("43 checado");
+    arrayChech.push("43");
   }
   if (d44.checked) {
     console.log("44 checado");
+    arrayChech.push("44");
   }
   if (d45.checked) {
     console.log("45 checado");
+    arrayChech.push("45");
   }
   if (d46.checked) {
     console.log("46 checado");
+    arrayChech.push("46");
   }
   if (d47.checked) {
     console.log("47 checado");
+    arrayChech.push("47");
   }
 }
